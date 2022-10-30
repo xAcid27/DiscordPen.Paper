212d3b530f4c82a797274a3a5c261584ab04e12d
xAcid <quotenskope@gmail.com>
2022-10-30 05:35:30 +0100

    Added Banksystem and some fixes DiscordPen.Paper v0.5

--- cogs/errorhandle.py
+++ cogs/errorhandle.py
@@ -10,13 +10,8 @@ class errorhandle(commands.Cog):  # Baseclass quasi Gerüst
 # Errorhandle
     @commands.Cog.listener()
     async def on_application_command_error(self, ctx, error):
-        if isinstance(error, commands.M):
-            await ctx.respond("Du hast nicht die benötigten Rechte dafür. :cry: ", ephemeral=True)
-            return
-
         await ctx.respond(f"Es ist ein Fehler aufgetreten!: ```{error}```")  #Ausgabe im Discordchat
         raise error  #Ausgabe in der Konsole
-
 
 
 def setup(bot):
